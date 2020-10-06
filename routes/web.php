ea2f7c35b1fca60d42f45cc0b46004438e1da1c8
Claudio Suarez <csuarez689@gmail.com>
2020-10-06 11:02:39 -0300

    test heroku

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return File::get(public_path() . '/index.html');
+    return "HOLA";
 });
