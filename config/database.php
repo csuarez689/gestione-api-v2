4779fcb319ae7b3fa0785030a706956bfc59a51d
Claudio Suarez <csuarez689@gmail.com>
2020-10-06 11:58:26 -0300

    generic database config

--- config/database.php
+++ config/database.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Support\Str;
 
-//for clearDB heroku
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+
 
 return [
 
@@ -51,13 +46,13 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
+            'driver' => env('DB_CONNECTION'),
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', $host),
+            'host' => env('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
