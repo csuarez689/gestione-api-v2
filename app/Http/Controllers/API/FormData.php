3a43017749b9cdbebf527047e11f34d8502c2ab2
Claudio Suarez <csuarez689@gmail.com>
2020-10-17 18:49:13 -0300

    added subjects, teacher_titles, schools_orientations

--- app/Http/Controllers/API/FormData.php
+++ app/Http/Controllers/API/FormData.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FormData extends Controller
 {
@@ -15,8 +16,12 @@ class FormData extends Controller
         'types' => \App\Models\SchoolType::class,
         'journey_types' => \App\Models\JourneyType::class,
         'job_states' => \App\Models\JobState::class,
-        'high_school_types' => \App\Models\HighSchoolType::class
+        'high_school_types' => \App\Models\HighSchoolType::class,
+        'subjects' => 'SELECT DISTINCT subject AS name FROM `schools_teachers` ORDER BY subject ASC',
+        'teacher_titles' => 'SELECT DISTINCT teacher_title AS name FROM `schools_teachers` ORDER BY teacher_title ASC',
+        'school_orientations' => 'SELECT DISTINCT orientation AS name FROM `schools` ORDER BY orientation ASC'
     ];
+
     /**
      * Handle the incoming request.
      *
@@ -28,9 +33,12 @@ class FormData extends Controller
         $data = [];
         if ($request->include) {
             $includes = explode(",", $request->include);
+
             foreach ($includes as $key => $value) {
                 if (array_key_exists($value, $this->formData)) {
-                    $data[$value] = $this->formData[$value]::orderBy('name')->get();
+                    $data[$value] = class_exists($this->formData[$value])
+                        ?  $this->formData[$value]::orderBy('name')->get()
+                        : DB::select($this->formData[$value]);
                 }
             }
         }
