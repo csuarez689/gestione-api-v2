94ae00e8f68949193cdb90b9883422d84fba52ba
Claudio Suarez <csuarez689@gmail.com>
2020-10-22 04:13:54 -0300

    added report for teaching plant

--- app/Http/Controllers/API/ReportsController.php
+++ app/Http/Controllers/API/ReportsController.php
@@ -14,41 +14,88 @@ class ReportsController extends BaseController
         $this->middleware('role:admin');
     }
 
+    //get amount of schools per provinces, departments an localities groups
+    //can be filtered by school type, sector or level
     public function schoolsCount(Request $request)
     {
-
         $sectorFilter = $request->query('sector_id') ? (int)$request->query('sector_id') : '%';
         $typeFilter = $request->query('type_id') ? (int)$request->query('type_id') : '%';
         $levelFilter = $request->query('level_id') ? (int)$request->query('level_id') : '%';
 
-        $departments = DB::table('schools')
+        $data = DB::table('schools')
+            ->select(DB::raw('count(schools.id) as total'), 'provinces.name as province', 'departments.name as department', 'localities.name as locality')
             ->join('localities', 'schools.locality_id', '=', 'localities.id')
             ->join('departments', 'localities.department_id', '=', 'departments.id')
             ->join('provinces', 'departments.province_id', '=', 'provinces.id')
-            ->select('provinces.name as province', 'departments.name as department', 'localities.name as locality', DB::raw('count(*) as total'))
+            ->where('provinces.id', '=', 74) //solo san luis
             ->where('schools.level_id', 'like', $levelFilter)
             ->where('schools.sector_id', 'like', $sectorFilter)
             ->where('schools.type_id', 'like', $typeFilter)
-            ->groupBy(['province', 'department', 'locality'])
+            ->groupBy('department', 'locality')
             ->get()
-            ->groupBy('department'); //For collection output
+            ->groupBy(['province', 'department']); //For collection output
 
-        $total_province = DB::table('schools')
-            ->where('schools.level_id', 'like', $levelFilter)
-            ->where('schools.sector_id', 'like', $sectorFilter)
-            ->where('schools.type_id', 'like', $typeFilter)->count();
+        $formatedData = new Collection();
 
-        $data = new Collection();
-
-        //Format output custom json
-        if ($total_province) {
-            $departments->each(function ($item, $key) use ($data) {
-                $total_department = $item->sum('total');
-                $data->push(["department" => $key, "total" => $total_department, "localities" => $item]);
+        // Format output custom json
+        $data->each(function ($item, $key) use ($formatedData) {
+            $departments = new Collection();
+            $item->each(function ($item, $key) use ($departments) {
+                $departments->push(["department" => $key, "total" => $item->sum('total'), "localities" => $item]);
             });
-            $data = ["total" => $total_province, "departments" => $data];
-        }
+            $formatedData->push(["province" => $key, "total" => $departments->sum('total'), "departments" => $departments]);
+        });
 
-        return response()->json($data);
+        return response()->json($formatedData);
+    }
+
+    //get amount of charges on teaching plant
+    //amount of peoples
+    //avg of charges per people
+    //grouped by provinces, departments and localities
+    public function teachingPlantCharges()
+    {
+        $data = DB::table('schools_teachers')
+            ->select(
+                DB::raw("count(DISTINCT schools_teachers.teacher_id) as peoples"),
+                DB::raw("count(schools_teachers.teacher_id) as charges"),
+                DB::raw("round(count(schools_teachers.teacher_id)/count(DISTINCT schools_teachers.teacher_id),2) as avg_charges_per_people"),
+                "provinces.name as province",
+                "departments.name as department",
+                "localities.name as locality"
+            )
+            ->join('schools', 'schools_teachers.school_id', '=', 'schools.id')
+            ->join('localities', 'schools.locality_id', '=', 'localities.id')
+            ->join('departments', 'localities.department_id', '=', 'departments.id')
+            ->join('provinces', 'departments.province_id', '=', 'provinces.id')
+            ->where('provinces.id', '=', 74) //solo san luis
+            ->groupBy('department', 'locality')
+            ->get()
+            ->groupBy(['province', 'department']); //For collection output
+
+        $formatedData = new Collection();
+
+        // Format output custom json
+        $data->each(function ($item, $key) use ($formatedData) {
+            $departments = new Collection();
+            $item->each(function ($item, $key) use ($departments) {
+                $departments->push([
+                    "department" => $key,
+                    "peoples" => $item->sum('peoples'),
+                    "charges" => $item->sum('charges'),
+                    "avg_charges_per_people" => number_format($item->sum('charges') / $item->sum('peoples'), 2),
+                    "localities" => $item
+                ]);
+            });
+            $formatedData->push([
+                "province" => $key,
+                "peoples" => $departments->sum('peoples'),
+                "charges" => $departments->sum('charges'),
+                "avg_charges_per_people" => number_format($departments->sum('charges') / $departments->sum('peoples'), 2),
+                "departments" => $departments
+            ]);
+        });
+
+        return response()->json($formatedData);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,8 @@ Route::group(['as' => 'api.', 'middleware' => 'auth:api'], function () {
     Route::delete('failedOrdenesMerito', [\App\Http\Controllers\API\FailedOrdenMeritoController::class, 'truncate'])->name('failedOrdenesMerito.truncate');
 
     //-------------REPORTS ROUTES-------------
-    Route::group(['prefix' => 'reports', 'as' => 'reports.', 'middleware' => 'role:admin'], function () {
+    Route::group(['prefix' => 'reports', 'as' => 'reports.'], function () {
         Route::get('schoolsCount', [\App\Http\Controllers\API\ReportsController::class, 'schoolsCount'])->name('schoolsCount');
+        Route::get('teachingPlantCharges', [\App\Http\Controllers\API\ReportsController::class, 'teachingPlantCharges'])->name('teachingPlantCharges');
     });
 });
