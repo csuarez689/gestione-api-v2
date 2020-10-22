17c15f75d94983133d6e13c1a54e77c1272c43b2
Claudio Suarez <csuarez689@gmail.com>
2020-10-22 18:25:10 -0300

    om inscriptions report

--- app/Http/Controllers/API/ReportsController.php
+++ app/Http/Controllers/API/ReportsController.php
@@ -59,7 +59,7 @@ class ReportsController extends BaseController
             ->select(
                 DB::raw("count(DISTINCT schools_teachers.teacher_id) as peoples"),
                 DB::raw("count(schools_teachers.teacher_id) as charges"),
-                DB::raw("round(count(schools_teachers.teacher_id)/count(DISTINCT schools_teachers.teacher_id),2) as avg_charges_per_people"),
+                DB::raw("round(count(schools_teachers.teacher_id)/count(DISTINCT schools_teachers.teacher_id),2) AS avg_charges_per_people"),
                 "provinces.name as province",
                 "departments.name as department",
                 "localities.name as locality"
@@ -95,6 +95,74 @@ class ReportsController extends BaseController
                 "departments" => $departments
             ]);
         });
+
+        return response()->json($formatedData);
+    }
+
+    //get amount of om inscriptions
+    //amount of peoples register on om
+    //avg of inscriptions per person
+    //grouped by provinces, departments and localities
+    //can be filtered by year
+    public function omInscriptions(Request $request)
+    {
+        $yearFilter = $request->query('year') ? (int)$request->query('year') : '%';
+
+        // SELECT orden_meritos.year,
+        // count(DISTINCT orden_meritos.cuil) as "personas",
+        // COUNT(*) as "inscripciones",
+        // round((COUNT(*) / COUNT(DISTINCT cuil)),2) as "avg_inscripcion_per_person",
+        // provinces.name as "province",
+        // departments.name as "department",
+        // localities.name as "locality"
+        // FROM `orden_meritos`
+        // join localities on localities.name=orden_meritos.locality
+        // join departments on localities.department_id=departments.id
+        // join provinces on departments.province_id=provinces.id
+        // GROUP BY orden_meritos.year,provinces.name,departments.name,localities.name
+
+        $data = DB::table('orden_meritos')
+            ->select(
+                DB::raw('COUNT(DISTINCT orden_meritos.cuil) AS persons'),
+                DB::raw('COUNT(*) AS inscriptions'),
+                DB::raw('ROUND((COUNT(*) / COUNT(DISTINCT cuil)),2) AS avg_inscriptions_per_person'),
+                'orden_meritos.year AS year',
+                'provinces.name AS province',
+                'departments.name AS department',
+                'localities.name AS locality_name'
+            )
+            ->join('localities', 'localities.name', '=', 'orden_meritos.locality')
+            ->join('departments', 'departments.id', '=', 'localities.department_id')
+            ->join('provinces', 'provinces.id', '=', 'departments.province_id')
+            ->where('orden_meritos.year', 'like', $yearFilter)
+            ->groupBy('province', 'department', 'locality_name')
+            ->get()
+            ->groupBy(['province', 'department']); //For collection output
+
+
+        $formatedData = new Collection();
+
+        // Format output custom json
+        $data->each(function ($item, $key) use ($formatedData) {
+            $departments = new Collection();
+            $item->each(function ($item, $key) use ($departments) {
+                $departments->push([
+                    "department" => $key,
+                    "persons" => $item->sum('persons'),
+                    "inscriptions" => $item->sum('inscriptions'),
+                    "avg_inscriptions_per_person" => number_format($item->sum('inscriptions') / $item->sum('persons'), 2),
+                    "localities" => $item
+                ]);
+            });
+            $formatedData->push([
+                "province" => $key,
+                "persons" => $departments->sum('persons'),
+                "inscriptions" => $departments->sum('inscriptions'),
+                "avg_inscriptions_per_person" => number_format($departments->sum('inscriptions') / $departments->sum('persons'), 2),
+                "departments" => $departments
+            ]);
+        });
+
 
         return response()->json($formatedData);
     }
--- routes/api.php
+++ routes/api.php
@@ -66,5 +66,6 @@ Route::group(['as' => 'api.', 'middleware' => 'auth:api'], function () {
     Route::group(['prefix' => 'reports', 'as' => 'reports.'], function () {
         Route::get('schoolsCount', [\App\Http\Controllers\API\ReportsController::class, 'schoolsCount'])->name('schoolsCount');
         Route::get('teachingPlantCharges', [\App\Http\Controllers\API\ReportsController::class, 'teachingPlantCharges'])->name('teachingPlantCharges');
+        Route::get('omInscriptions', [\App\Http\Controllers\API\ReportsController::class, 'omInscriptions'])->name('omInscriptions');
     });
 });
