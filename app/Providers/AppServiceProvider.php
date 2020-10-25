0383f0237eaf68e9cf4e5b7cfa3c6ae1068cdd54
Claudio Suarez <csuarez689@gmail.com>
2020-10-25 18:12:25 -0300

    string length database config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\OrdenMerito;
 use App\Observers\OrdenMeritoObserver;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         JsonResource::withoutWrapping();
         OrdenMerito::observe(OrdenMeritoObserver::class);
     }
